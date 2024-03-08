bf466565d4871ecaa4424b25e9c87b0d6f0b7a51
Jason Suarez <swarecito@gmail.com>
2024-03-08 18:07:50 +0100

    📦 NEW: Add painpoints section and adjust layout in hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,9 +3,9 @@ import Header from "./Header";
 
 export default function Hero() {
   return (
-    <section className="hero w-screen  lg:h-screen">
+    <section className="hero w-screen  xl:h-screen">
       <Header />
-      <div className="max-w-8xl mx-auto inline-block justify-center  p-3 pt-0 lg:flex lg:flex-wrap items-center lg:pt-4">
+      <div className="max-w-8xl w-full mx-auto inline-block justify-center  p-3 pt-0 lg:flex lg:flex-wrap items-center lg:pt-4">
         <div className="lg:w-3/6 pb-12 xl:pb-0 h-full flex flex-col justify-center items-center">
           <h2 className="primary-color text-left max-w-xl text-5xl  lg:text-7xl font-bold leading-none text-black inline-block">
             Carré Style
@@ -16,12 +16,12 @@ export default function Hero() {
           </p>
           <ContactButton className="pt-8 hidden xl:inline-block" />
         </div>
-        <div className="  hidden w-full flex-col  lg:inline-block lg:w-3/6">
-          <img src="/images/beton-hero2.jpg" width={"600px"} alt="Hero" />
-        </div>
-        <div className=" inline-block w-full flex-col lg:mt-0 lg:hidden lg:w-2/5">
-          <img src="/images/beton-hero2.jpg" alt="image" />
-        </div>
+        <img
+          src="/images/beton-hero2.jpg"
+          width={"600px"}
+          className="xl:w-3/6"
+          alt="Hero"
+        />
       </div>
       <div className="flex z-10 justify-center py-12  items-center h-max w-screen">
         <ContactButton className="xl:hidden" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import { BenefitsObjectionsData } from "./components/Painpoints";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <BenefitsObjectionsData />
       <Footer />
     </>
   );
