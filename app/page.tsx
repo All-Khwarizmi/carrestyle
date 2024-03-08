70796ee760012a3df2233066b7399500e17e9f45
Jason Suarez <swarecito@gmail.com>
2024-03-08 14:28:17 +0100

    📦 NEW: Refactor hero section into a component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Layout from "./components/Layout";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Layout />
+      <Hero />
       <Footer />
     </>
   );
