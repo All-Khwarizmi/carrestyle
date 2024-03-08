f505271ab4a5602e25a0197ac6b7a12a86d1a701
Jason Suarez <swarecito@gmail.com>
2024-03-08 14:16:49 +0100

    📦 NEW: Add head fil with corresponding metadata

---
+++ app/components/Head.tsx
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <>
+      <title>Carréstyle</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
+      />
+      
+      <meta
+        name="description"
+        content="Carréstyle - le style de toutes vos envies"
+      />
+      <link rel="icon" href="./public/favicon.ico" />
+    </>
+  );
+}
