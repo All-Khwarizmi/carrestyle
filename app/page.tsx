5a91b21b8bd2fbeb3b9520de07410d2f9516a08f
Jason Suarez <swarecito@gmail.com>
2024-03-08 15:01:52 +0100

    📦 NEW: Update hero section: change hero image and text size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Layout from "./components/Layout";
 import Hero from "./components/Hero";
 
 export default function Home() {
