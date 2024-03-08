e31f8a361045b18e42379b967bc7ccb40db9017a
Jason Suarez <swarecito@gmail.com>
2024-03-08 19:35:48 +0100

    📦 NEW: add social network metatags

--- app/components/Head.tsx
+++ app/components/Head.tsx
@@ -6,12 +6,45 @@ export default function Head() {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
       />
-      
+
       <meta
         name="description"
         content="Carréstyle - le style de toutes vos envies"
       />
       <link rel="icon" href="./public/favicon.ico" />
+      {/*      HTML Meta Tags */}
+      <title>Carréstyle</title>
+      <meta
+        name="description"
+        content="Carréstyle - le style de toutes vos envies"
+      />
+
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:url" content="https://carrestyle.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Carréstyle" />
+      <meta
+        property="og:description"
+        content="Carréstyle - le style de toutes vos envies"
+      />
+      <meta
+        property="og:image"
+        content="https://opengraph.b-cdn.net/production/documents/62e2b1bc-12a3-4c2a-9e50-0ebc2f2d0f65.png?token=tJ51wzYyZ2OxtqrY9KJ97RaluixYHcCihjVQOasVrWw&height=1200&width=1200&expires=33245922596"
+      />
+
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="carrestyle.vercel.app" />
+      <meta property="twitter:url" content="https://carrestyle.vercel.app/" />
+      <meta name="twitter:title" content="Carréstyle" />
+      <meta
+        name="twitter:description"
+        content="Carréstyle - le style de toutes vos envies"
+      />
+      <meta
+        name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/documents/62e2b1bc-12a3-4c2a-9e50-0ebc2f2d0f65.png?token=tJ51wzYyZ2OxtqrY9KJ97RaluixYHcCihjVQOasVrWw&height=1200&width=1200&expires=33245922596"
+      />
     </>
   );
 }
